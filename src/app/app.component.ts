bfddd1758a05e086ee9171b401544505a7aec4d7
dilipkumarkm7 <dilip72765@gmail.com>
2025-03-10 11:51:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import {CdkVirtualScrollViewport, ScrollingModule } from '@angular/cdk/scrolling';
+import { CommonModule } from '@angular/common';
+
+interface user{
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CdkVirtualScrollViewport, ScrollingModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'optimizedList';
+export class AppComponent implements OnInit{
+  users: user[] = [];
+  totalNumbers: number = 10000;
+  constructor(private cdr: ChangeDetectorRef){}
+  ngOnInit(): void {
+    this.users = Array.from({length: this.totalNumbers}, (_, index) => ({
+      id: index + 1,
+      name: `User ${index + 1}`,
+      email: `user${index + 1}@example.com`,
+      role: index % 5 === 0 ? 'Admin' : 'User'
+    }));
+  }
+  loadUsers(){
+    this.cdr.markForCheck();
+  }
 }
